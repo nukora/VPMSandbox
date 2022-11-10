4abef6a983e20e939ab59f5eaf0eb804dea56406
nukora <jp.nukora@gmail.com>
2022-11-10 17:20:34 +0900

    てすと

---
+++ Packages/jp.nukora.vpmsandbox/README.md
@@ -0,0 +1,1 @@
+# jp.nukora.vpmsandbox by nukora
---
+++ Packages/jp.nukora.vpmsandbox/Runtime/Scripts/Switch.cs
@@ -0,0 +1,18 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+namespace Nukora.VPMSandbox
+{
+    public class Switch : UdonSharpBehaviour
+    {
+        public GameObject target;
+
+        public override void Interact()
+        {
+            target.SetActive(!target.activeSelf);
+        }
+    }
+}
